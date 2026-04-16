b7271e692b3641b886f4fc256682f72f5e2fc77c
michaelguzman-boop <michael.guzman@allcaretherapies.com>
2026-04-16 12:02:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+
+app.post("/slack/events", (req, res) => {
+  const data = req.body;
+
+  // 🔥 ESTO ES LO QUE SLACK NECESITA
+  if (data.type === "url_verification") {
+    return res.send(data.challenge);
+  }
+
+  return res.sendStatus(200);
+});
 
 app.get("/", (req, res) => {
-  res.send("Bot funcionando 🚀");
+  res.send("Bot activo 🚀");
 });
 
 app.listen(3000, () => {
