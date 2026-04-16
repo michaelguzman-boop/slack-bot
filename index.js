e0a8ebe0fd45d701d5a7a6cce2ec6566f5d0b6d4
michaelguzman-boop <michael.guzman@allcaretherapies.com>
2026-04-16 12:05:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ app.get("/", (req, res) => {
   res.send("Bot activo 🚀");
 });
 
-app.listen(3000, () => {
-  console.log("Server running");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
